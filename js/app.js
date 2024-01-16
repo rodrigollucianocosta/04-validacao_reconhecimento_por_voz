32cd80019a56ca188c8a1abf7cf8c4942d2b29c8
Rodrigo.Luciano <brdc.cxlviii1@huawei.com>
2024-01-16 12:49:32 -0300

    ajuste de interface

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const menorValor = 1
+const maiorValor = 1000
+const numeroSecreto = gerarNumeroAleatorio()
+
+function gerarNumeroAleatorio(){
+    return parseInt(Math.random() * maiorValor + 1)
+}
+console.log('Numero Secreto:', numeroSecreto)
+
+const elementoMenorValor = document.getElementById('menor-valor')
+const elementoMaiorValor = document.getElementById('maior-valor')
+
+elementoMenorValor.innerHTML = menorValor
+elementoMaiorValor.innerHTML = maiorValor
