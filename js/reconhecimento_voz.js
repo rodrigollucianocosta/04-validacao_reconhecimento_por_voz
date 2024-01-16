eaff23cf4f4084c734fb31548e7bbcf48168b237
Rodrigo.Luciano <brdc.cxlviii1@huawei.com>
2024-01-16 17:02:10 -0300

    update project

--- js/reconhecimento_voz.js
+++ js/reconhecimento_voz.js
@@ -1,21 +1,24 @@
 const elementoChute = document.getElementById('chute')
 
 window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
-const recognition = new SpeechRecognition();
 
+const recognition = new SpeechRecognition();
 recognition.lang = 'pt-Br'
 recognition.start()
 
-recognition.addEventListener('result',onSpeak)
+recognition.addEventListener('result', onSpeak)
 
 function onSpeak(e) {
     chute = e.results[0][0].transcript
     exibeChuteNaTela(chute)
+    verificaSeOChutePossuiUmValorValido(chute)
 }
 
-function exibeChuteNaTela(chute){
+function exibeChuteNaTela(chute) {
     elementoChute.innerHTML = `
-        <div>Voce disse></div>
+        <div>Você disse</div>
         <span class="box">${chute}</span>
-    `
+     `
 }
+
+recognition.addEventListener('end', () => recognition.start())
